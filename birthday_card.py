4fd88687f17f083da5560c23f57d97e59223bc4d
Sam <you@example.com>
2024-02-11 20:45:09 +0000

    Initial commit

---
+++ birthday_card.py
@@ -0,0 +1,24 @@
+# This is the personal Information
+recipient_name = 'Sam'
+birth_year = 1996
+personalized_message = '''
+And hope you can pursue what you really like and become the person that you want to be. Wish to see you soon!'''
+sender_name = 'jay'
+
+# The definition of current year
+current_year = 2024
+
+# The calculation of the recipient's age
+age = current_year - birth_year
+
+# Print birthday card
+print("\nBirthday card:\n")
+print(f"{recipient_name}, let's celebrate your {age} years of awesomeness!") 
+print(f"Wishing you a day filled with joy and laughter as you turn {age}!")
+print(personalized_message)
+print("\nWith love and best wishes,")
+print(sender_name)
+
+
+      
+      
