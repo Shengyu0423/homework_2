87bc78936e7e51dc6e1bb75681d705e81776fca9
Sam <you@example.com>
2024-02-12 20:50:50 +0000

    revised version 1

--- birthday_card.py
+++ birthday_card.py
@@ -1,9 +1,8 @@
 # This is the personal Information
-recipient_name = 'Sam'
-birth_year = 1996
-personalized_message = '''
-And hope you can pursue what you really like and become the person that you want to be. Wish to see you soon!'''
-sender_name = 'jay'
+recipient_name = input("Tell me the recipent's name: ")
+birth_year = int(input ("Which year you were born? "))
+personalized_message = input("Anything that you would like to say? ")
+sender_name = input("Tell me the sender's name? ")
 
 # The definition of current year
 current_year = 2024
@@ -12,9 +11,8 @@ current_year = 2024
 age = current_year - birth_year
 
 # Print birthday card
-print("\nBirthday card:\n")
-print(f"{recipient_name}, let's celebrate your {age} years of awesomeness!") 
-print(f"Wishing you a day filled with joy and laughter as you turn {age}!")
+print(f"\n{recipient_name}, let's celebrate your {age} years of awesomeness!") 
+print(f"Wishing you a day filled with joy and laughter as you turn {age}!\n")
 print(personalized_message)
 print("\nWith love and best wishes,")
 print(sender_name)
