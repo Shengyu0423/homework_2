f8b2f082f420cdbacc2ca0aea3b580b3bd344837
Sam <you@example.com>
2024-03-15 00:59:24 +0000

    fifth_assignment_revised

--- school_management_software.py
+++ school_management_software.py
@@ -1,21 +1,3 @@
-class Student:
-    def __init__(self, first_name, last_name, class_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.class_name = class_name
-
-class Teacher:
-    def __init__(self, first_name, last_name, class_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.class_name = class_name
-
-class HomeroomTeacher:
-    def __init__(self, first_name, last_name, class_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.class_name = class_name
-
 database = {
     "students": [
         {"first_name": "John", "last_name": "Smith", "class": "3C"},
@@ -30,6 +12,53 @@ database = {
     ]
 }
 
+def create_student(database, first_name, last_name, class_name):
+    new_student = {"first_name": first_name, "last_name": last_name, "class": class_name}
+    database["students"].append(new_student)
+    print("\nCreate the new student successfully!")
+
+    return database
+
+def create_homeroom_teacher(database, first_name, last_name, class_name):
+    new_homeroom_teacher = {"first_name": first_name, "last_name": last_name, "class": class_name}
+    database["homeroom_teachers"].append(new_homeroom_teacher)
+    print("\nCreate the new homeroom teacher successfully!")
+
+    return database
+
+def show_class(database, class_name):
+    class_found = False
+    for student in database["students"]:
+        if student["class"] == class_name:
+            class_found = True
+            print("Students: ")
+            for stu in database["students"]:
+                if stu["class"] == class_name:
+                    print(f"- {stu['first_name']} {stu['last_name']}")
+                        
+            print("Homeroom Teacher: ")
+            for homeroom_teacher in database["homeroom_teachers"]:
+                if homeroom_teacher["class"] == class_name:
+                    print(f" - {homeroom_teacher['first_name']} {homeroom_teacher['last_name']}")
+                else:
+                    print("- No homeroom teacher at this stage.")
+            break
+                
+    if not class_found:
+        print("\nError: Class not found in the database!")
+
+def show_student(database, first_name, last_name):
+    for student in database["students"]:
+        if student["first_name"] == first_name and student["last_name"] == last_name:
+            print(f"\nStudent: {first_name} {last_name} attends class {student["class"]}")
+            for teacher in database["teachers"]:
+                if student["class"] in teacher["classes"]:
+                    print(f"Teacher for this class: {teacher["first_name"]} {teacher["last_name"]}")
+            break
+    else:
+        print("\nError: Student is not in the database!")
+
+                        
 while True:
     print("\nHello! Welcome to Future School Management Software system!")
     print("\nPlease input one of following commands: create, manage, end.")
@@ -44,10 +73,8 @@ while True:
             last_name = input("\nEnter the last name of the student: ")
             class_name = input("\nEnter the class name: ")
 
-            # Add new student to database
-            new_student = {"first_name": first_name, "last_name": last_name, "class": class_name}
-            database["students"].append(new_student)
-            print("\nCreate the new student successfully!")
+            database = create_student(database, first_name, last_name, class_name)
+
         elif category == "teacher":
             print("\nPlease provide the following information of the teacher: ")
             first_name = input("\nEnter the first name of the teacher: ")
@@ -72,10 +99,9 @@ while True:
             last_name = input("\nEnter the last name of the homeroom teacher: ")
             class_name = input("\nEnter the class name of the homeroom teacher leads: ")
 
-            # Add new homeroom teacher to database
-            new_homeroom_teacher = {"first_name": first_name, "last_name": last_name, "class": class_name}
-            database["homeroom_teachers"].append(new_homeroom_teacher)
-            print("\nCreate the new homeroom teacher successfully!")
+            database = create_homeroom_teacher(database, first_name, last_name, class_name)
+
+            
         elif category == "end":
             print("\nReurn to the main menu.")
             continue
@@ -85,42 +111,20 @@ while True:
     elif command == "manage":
         print("\nPlease choose one of categories that you want to manage: class, student, teacher, homeroom teacher, end.")
         category = input("\nEnter a category: ").lower()
+
         if category == "class":
-            while True:
-                class_number = input("\nPlease input the class number: ")
+            class_name = input("\nPlease input the class name: ")
 
-                # check if the class number exists in the database
-                class_exists = any("class" in student and student["class"] == class_number for student in database["students"]) or any("class" in teacher and teacher["class"] == class_number for teacher in database["teachers"])
-
-                if class_exists:
-                    break
-                else:
-                    print("\nError: Class not found in the database. Please enter a valid class number.")
+            show_class(database, class_name)
                 
-            students_in_class = [student for student in database["students"] if student["class"] == class_number]
-            print("\nStudents in class", class_number)
-            for student in students_in_class:
-                print(f"{student["first_name"]} {student["last_name"]}")
 
-            homeroom_teacher = next((teacher for teacher in database["homeroom_teachers"] if teacher["class"] == class_number), None)
-            if homeroom_teacher:
-                print(f"\nHomeroom teacher for class {class_number}: {homeroom_teacher["first_name"]} {homeroom_teacher["last_name"]}")
-            else:
-                print("\nNo homeroom teacher assigned for class", class_number)
-        
         elif category == "student":
             print("\nPlease provide the following information of the student: ")
             first_name = input("\nEnter the first name of the student: ")
             last_name = input("\nEnter the last name of the sudent: ")
-            for student in database["students"]:
-                if student["first_name"] == first_name and student["last_name"] == last_name:
-                    print(f"\nStudent: {first_name} {last_name} attends class {student["class"]}")
-                    for teacher in database["teachers"]:
-                        if student["class"] in teacher["classes"]:
-                            print(f"Teacher for this class: {teacher["first_name"]} {teacher["last_name"]}")
-                    break
-            else:
-                print("\nError: Student is not in the database!")
+
+            show_student(database, first_name, last_name)
+
 
         elif category == "teacher":
             print("\nPlease provide the following information of the teacher: ")
@@ -158,7 +162,6 @@ while True:
          break
     else:
          print("\nError: Please enter a valid command!")
-
                           
 
                      
