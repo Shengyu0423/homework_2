43faf5634c05c3987c38aaeedc3eef558464f6e4
Sam <you@example.com>
2024-03-18 23:20:22 +0000

    sixth_assignment

---
+++ accounting_system_warehouse_with_a_text_database/functions.py
@@ -0,0 +1,26 @@
+def save_balance_to_file(balance, filename):
+    try:
+        with open(filename, 'w') as file:
+            file.write(str(balance))
+        print("Account balance saved successfully!")
+    except Exception as e:
+        print(f"Error saving account balance: {e}")
+
+def save_inventory_to_file(inventory, filename):
+    try:
+        with open(filename, 'w') as file:
+            for product, details in inventory.items():
+                file.write(f"Product name: {product}, Price: {details['price']}, Quantities: {details['quantities']}\n")
+        print("Warehouse inventory saved successfully!")
+    except Exception as e:
+        print(f"Error saving warehouse inventory: {e}")
+
+def save_operations_to_file(operations, filename):
+    try:
+        with open(filename, 'w') as file:
+            for operation in operations:
+                operation_type, product_name, price, quantity = operation
+                file.write(f"Type: {operation_type}, Product Name: {product_name}, Price: {price}, Quantities: {quantity}\n")
+        print("Recorded operations saved successfully!")
+    except Exception as e:
+        print(f"Error saving recorded operations: {e}")
---
+++ accounting_system_warehouse_with_a_text_database/main.py
@@ -0,0 +1,159 @@
+from functions import save_balance_to_file, save_inventory_to_file, save_operations_to_file
+
+warehouse_inventory = {
+    "monster-original": {"price": 2, "quantities": 50},
+    "monster-passionfruit": {"price": 2.5, "quantities": 30},
+    "monster-mango": {"price": 2.5, "quantities": 30},
+    "monster-kiwi": {"price": 3, "quantities": 20},
+    "monster-citrus": {"price": 3.5, "quantities": 15}
+}
+
+account_balance = 1000
+
+recorded_operations = [] #save all the operation
+
+while True:
+    print("\nWelcome to the Monster Beverage Corporation!")
+    print("\nAvailable commands: balance, sale, purchase, account, list, warehouse, review, end")
+    command = input("\nEnter a command: ").lower()
+
+    if command == "balance":
+        try:
+            amount = float(input("\nPlease add or subtract the amount to the account: "))
+            account_balance += amount
+            print(f"\nUpdated account balance: {account_balance}")
+        except ValueError:
+            print("\nError: Please enter a valid amount.")
+
+    elif command == "sale":
+        try:
+            product_name = input("\nPlease enter the product name: ")
+            if product_name in warehouse_inventory:
+                price = float(input("\nPlease enter the price per unit: "))
+                quantity = int(input("\nPlease enter the quantity sold: "))
+
+                if warehouse_inventory[product_name]["quantities"] >= quantity:
+                    total_sale_amount = price * quantity
+                    account_balance += total_sale_amount
+
+                    # Update the warehouse inventory
+                    warehouse_inventory[product_name]["quantities"] -= quantity
+                    print("\nSale completed successfully!")
+
+                    # Record the operation
+                    recorded_operations.append(("sale", product_name, price, quantity))
+                else:
+                    print("\nError: Insufficient inventory for sale!")
+            else:
+                print("\nError: Invalid product name!")
+        except ValueError:
+            print("\nPlease enter a valid input for product name, price, and quantity.")
+
+    elif command == "purchase":
+        try:
+            product_name = input("\nPlease enter the product name: ")
+            if product_name in warehouse_inventory:
+                price = float(input("\nPlease enter the price per unit: "))
+                quantity = int(input("\nPlease enter the quantity to purchase: "))
+
+                if quantity > 0:
+                    total_purchase_cost = price * quantity
+                    if account_balance >= total_purchase_cost:
+                        # Update account balance
+                        account_balance -= total_purchase_cost
+
+                        # Update warehouse inventory
+                        warehouse_inventory[product_name]["quantities"] += quantity
+                        print("\nPurchase completed successfully!")
+
+                        # Record the operation
+                        recorded_operations.append(("purchase", product_name, price, quantity))
+                    else:
+                        print("\nError: Insufficient account balance for purchase!")
+                else:
+                    print("\nError: Invalid quantity for purchase!")
+            else:
+                # Ask user to provide details of new product
+                print("\nProduct not found in inventory. Please provide the following information:")
+                price = float(input("\nEnter the price per unit: "))
+                quantity = int(input("\nEnter the initial quantity: "))
+
+                if quantity > 0:
+                    total_purchase_cost = price * quantity
+                    if account_balance >= total_purchase_cost:
+                        # Update account balance
+                        account_balance -= total_purchase_cost
+
+                        # Add new product to inventory
+                        warehouse_inventory[product_name] = {"price": price, "quantities": quantity}
+                        print("\nNew product added to inventory successfully!")
+                        
+                        # Record the operation
+                        recorded_operations.append(("purchase", product_name, price, quantity))
+                    else:
+                        print("\nError: Insufficient account balance for purchase!")
+                else:
+                    print("\nError: Inalid quantity for purchase!")
+
+        except ValueError:
+            print("\nError: Please enter valid inputs for price and quantity. ")
+
+    elif command == "account":
+        print(f"\nCurrent account balance: {account_balance}")
+
+    elif command ==  "list":
+        print("\nWarehouse Inventory:\n ")
+        for item, details in warehouse_inventory.items():
+            print(f"{item.capitalize()}:")
+            for detail, value in details.items():
+                print(f"- {detail.capitalize()}: {value}")
+
+    elif command == "warehouse":
+        product_name = input("\nPlease enter one of the products to check details: ")
+        if product_name in warehouse_inventory:
+            print(f"\nProduct: {product_name.capitalize()}")
+            for detail, value in warehouse_inventory[product_name].items():
+                print(f"- {detail.capitalize()}: {value}")
+        else:
+            print("\nError: Product not found in warehouse! ")
+
+    elif command == "review":
+        try:
+            from_index = input("\nEnter the 'from' index: ")
+            to_index = input("\nEnter the 'to' index: ")
+
+            if from_index == "":
+                from_index = 1
+            else:
+                from_index = int(from_index)
+
+            if to_index == "":
+                to_index = len(recorded_operations)
+            else:
+                to_index = int(to_index)
+
+            if from_index < 1 or to_index > len(recorded_operations) or from_index > to_index:
+                print("\nError: Invalid index range. ")
+            else:
+                print("\nRecorded operations within the specified range: ")
+                count = 1
+                for i in range(from_index - 1, to_index):
+                    operation = recorded_operations[i]
+                    operation_type, product_name, price, quantity = operation
+                    print(f"\n{count}. \n- Type: {operation_type} \n- Product: {product_name} \n- Price: {price} \n- Quantity:{quantity}")
+                    count += 1
+        
+        except ValueError:
+            print("\nError: Please enter a valid indices. ")
+            
+    elif command == "end":
+        # Save data to files before exiting
+        save_balance_to_file(account_balance, "balance.txt")
+        save_inventory_to_file(warehouse_inventory, "inventory.txt")
+        save_operations_to_file(recorded_operations, "operations.txt")
+        print("\nGoodbye! Hope you have a nice day! ")
+        break
+    else:
+        print("\nError: Please enter a valid command!")
+
+
