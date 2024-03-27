ba24a056e4b8f01fe336c6ce048f5fca519a2cf4
Sam <you@example.com>
2024-03-27 22:44:32 +0000

    seventh_assignment_first_revised

--- program_for_editing_csv/reader.py
+++ program_for_editing_csv/reader.py
@@ -9,21 +9,33 @@ if len(sys.argv) < 4:
 # Read changes from cmd
 in_csv = sys.argv[1]
 out_csv = sys.argv[2] 
-change_values = sys.argv[3].split(',') 
-
-x, y, new_value = int(change_values[0]), int(change_values[1]), change_values[2]
+change_values = sys.argv[3:]
 
 # Reading the original data from in.csv file
-with open('in.csv', 'r', newline='') as infile:
-    reader = csv.reader(infile)
+with open('in.csv', 'r', newline='') as file:
+    reader = csv.reader(file)
     data = list(reader)
 
+# Applying changes
+for change in change_values:
+    change_parts = change.split(',')
+    if len(change_parts) != 3:
+        print(f"Invalid change format: {change}. Correct format: x, y, new_value")
+        continue
 
-data[y][x] = new_value
+    try:
+        x, y, new_value = change_parts
+        x, y = int(x), int(y)
+        if x < 0 or y < 0 or y >= len(data) or x >= len(data[0]):
+            print(f"Change {change} is out of range.")
+            continue
+        data[y][x] = new_value
+    except ValueError:
+        print(f"Invalid change format: {change}. Correct format: x, y,new_value")
 
 # Writing the change into out.csv file
-with open('out.csv', 'w', newline='') as outfile:
-    writer = csv.writer(outfile)
+with open('out.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
     writer.writerows(data)
 
 # Reading the change from out.csv file
